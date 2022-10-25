a909e1f0ef2286356b46be8a7524ae112a6c1893
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-10-25 21:37:23 +0200

    création composant bouton

---
+++ src/components/Bouton.vue
@@ -0,0 +1,5 @@
+<template>
+    <button class="flex items-center justify-center bg-violet_foncé gap-2 rounded-lg p-5 text-[18px] md:text-[20px] xl:text-[28px] ">
+        <span class="text-white font-semibold font-work-sans"><slot/></span>
+    </button>
+</template>
