c64925ffdd1a900bdf07109a6cd3052ff8946a5c
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-10-25 16:33:02 +0200

    Intégration header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ const formKitTailwind = require("@formkit/themes/tailwindcss");
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+ colors: {
+  "violet_foncé": "#710697",
+  "Noir": "#000000",
+  "Blanc": "#ffffff"
+ },
+ "fontSize": {
+  "base": "1.5625rem",
+  "lg": "2.5rem"
+ },
+ fontFamily: {
+  "work-sans": "Work Sans",
+  "eb-garamond": "EB Garamond"
+ },
+ borderRadius: {
+  "none": "0",
+  "xs": "0.625rem",
+  "full": "9999px"
+}
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
