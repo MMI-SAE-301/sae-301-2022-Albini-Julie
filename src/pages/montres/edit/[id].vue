255ac78eff83267378f7f9da5ee3079b85104c38
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-10-26 11:54:59 +0200

    création id vue

---
+++ src/pages/montres/edit/[id].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import SvgMontre from "../../../components/SvgMontre.vue";
+defineProps<{
+  id: string;
+}>();
+</script>
+<template>
+  <SvgMontre :id="id" />
+</template>
