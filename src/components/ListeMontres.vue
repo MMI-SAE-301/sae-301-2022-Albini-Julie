8bb59e594f190358874c9dd0ebf1dac9cb9a2f4d
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2022-10-26 11:54:34 +0200

    Création de la liste des montres

---
+++ src/components/ListeMontres.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { supabase } from "@/supabase";
+import SvgMontre from "./SvgMontre.vue";
+import bouton from "../components/Bouton.vue"
+import { useRouter } from "vue-router";
+import type { Montre } from "@/types";
+import {ref} from "vue";
+const router = useRouter();
+
+
+
+const props = defineProps<{
+  max?: number;
+  data?: Montre;
+  id?: string;
+}>();
+
+const { data: montres, error } = await supabase
+  .from("montre")
+  .select("*")
+  .limit(props.max ?? 100);
+if (error) {
+  console.log("n'a pas pu récupérer les montres", { error });
+}
+
+const montre = ref<Montre>(props.data ?? {});
+
+if (props.id) {
+ // On charge les données du quartier
+ let { data, error } = await supabase
+ .from("montre")
+ .select("*")
+ .eq("id_montre", props.id);
+ if (error) console.log("n'a pas pu charger le table Montre :", error);
+ else montre.value = (data as any[])[0]};
+
+
+</script>
+<template>
+  <ul class="md:flex-nowrap  md:relative md:w-full flex md:snap-x md:overflow-x-auto pb-14">
+    <li class="flex flex-col justify-center items-center" v-for="montre in montres" :key="montre.id">
+      <div class="w-[600px]">
+      <router-link
+        :to="{ name: 'montres-edit-id', params: { id: montre.id_montre } }"
+      >
+        <SvgMontre class="w-[80%] snap-center shrink-0" v-bind="montre" />
+      </router-link>
+      </div>
+      <bouton class="w-1/2" @click= "montre.commande = true">Commander</bouton>
+    </li>
+  </ul>
+</template>
